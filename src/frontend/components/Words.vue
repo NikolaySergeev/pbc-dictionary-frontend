9d0f1b96f6f16b0d4fa91aea0a1a38de4e5e1af4
Nikolay Sergeev <sergeev.n.s@gmail.com>
2022-05-29 16:03:33 +0300

    Добавление исправлений

--- src/frontend/components/Words.vue
+++ src/frontend/components/Words.vue
@@ -2,44 +2,17 @@
     <div>
         <h1>Words</h1>
         <span>{{ msg }}</span>
-        <ul id="termsListId">
-          <li v-for="item in termsList" :key="item.title">
-            {{ item.title }}
-          </li>
-        </ul>
     </div>
 </template>
 
 <script>
-import axios from 'axios';
-
 export default {
   name: 'Words',
   data () {
     return {
-      msg: 'This is words list!',
-      termsList: [],
+      msg: 'This is words list!'
     }
-  },
-  mounted() {
-    axios.get('https://dictionarypy.herokuapp.com/terms/')
-/*
-      const getURL = 'https://dictionarypy.herokuapp.com/terms/';
-      const getInit = {
-	method: 'GET',
-	mode: 'no-cors',
-      };
-      const myRequest = new Request(getURL, getInit);
-      fetch(myRequest)
-*/
-      .then(response => { 
-        this.termsList = response
-        console.log(response)
-      })
-      .catch(err => {
-        console.log(err)
-      })
-  },
+  }
 }
 </script>
 
