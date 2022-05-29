173471d40bf13691d1253da74ec71a79bf1ca7f9
typhoon <typhoon@iMac-Dmitrij.local>
2022-05-29 22:16:08 +1000

    init axios
    updated

--- src/frontend/router/index.js
+++ src/frontend/router/index.js
@@ -2,8 +2,11 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from 'frontend/pages/Home.vue'
 import Profile from 'frontend/pages/Profile.vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
 Vue.use(Router)
+Vue.use(VueAxios, axios)
 
 export default new Router({
   routes: [
